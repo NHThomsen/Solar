1f9a9032b23a42f7a551d2781e3d0763e494a26c
philipv99 <philipv9913@gmail.com>
2023-05-22 10:04:25 +0200

    add page

---
+++ Solar/Solar/Pages/Ekstern/Opret_bruger.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Solar.Pages.Ekstern
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
