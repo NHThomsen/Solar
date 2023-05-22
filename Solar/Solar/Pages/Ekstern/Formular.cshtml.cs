96d838a6b013d3fadcf31bbe5db582135def4e07
philipv99 <philipv9913@gmail.com>
2023-05-22 11:57:47 +0200

    formular

---
+++ Solar/Solar/Pages/Ekstern/Formular.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Solar.Pages.Ekstern
+{
+    public class FormularModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Solar/Solar/Pages/Ekstern/Logind.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace Solar.Pages.Ekstern
+{
+    public class LogindModel : PageModel
+    {
+        private IUsersDataService _service;
+
+        public LogindModel(IUsersDataService userdata)
+        {
+            _service = userdata;
+        }
+
+        public static User LogedinUser { get; set; }
+
+        [BindProperty]
+        public string UserName { get; set; }
+
+        [BindProperty, DataType(DataType.Password)]
+        public string Password { get; set; } 
+
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            LogedinUser = _service.VerifyUser(UserName, Password);
+
+            if (LogedinUser == null)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
