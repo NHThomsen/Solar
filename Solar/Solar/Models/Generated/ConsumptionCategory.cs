04a3bed497580352d444969e5f2eeea015dfceee
nhthomsen <nooblas@hotmail.com>
2023-05-23 09:52:50 +0200

    Added changes to the data model

--- Solar/Solar/Models/Generated/ConsumptionCategory.cs
+++ Solar/Solar/Models/Generated/ConsumptionCategory.cs
@@ -6,7 +6,6 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-
 [Table("ConsumptionCategory")]
 public partial class ConsumptionCategory
 {
