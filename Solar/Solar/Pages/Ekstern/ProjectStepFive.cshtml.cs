e9c4f47f82097ab61c1ea843a5ffc58da8881b2b
philipv99 <philipv9913@gmail.com>
2023-05-24 14:58:57 +0200

    side 5 lavet

--- Solar/Solar/Pages/Ekstern/ProjectStepFive.cshtml.cs
+++ Solar/Solar/Pages/Ekstern/ProjectStepFive.cshtml.cs
@@ -6,10 +6,21 @@ namespace Solar.Pages.Ekstern
 {
     public class ProjectStepFiveModel : PageModel
     {
+        [BindProperty]
+        public Project ProjectData { get; set; }
+        public Project ExistingData { get; set; }
+        public Project InfoDump { get; set; }
+
+        public ProjectStepFiveModel()
+        {
+            ExistingData = GlobalProjectDataService.ProjectDataNewProject;
+            InfoDump = GlobalProjectDataService.Merge();
+        }
 
         public void OnGet()
         {
-            Project pro = GlobalProjectDataService.Merge();
+            
+           
         }
 
     }
