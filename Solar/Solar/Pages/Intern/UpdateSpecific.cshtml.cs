402e0424cdb863cff53dd620c040151e2ab2b9db
philipv99 <philipv9913@gmail.com>
2023-06-05 13:33:57 +0200

    edit ui intern side

---
+++ Solar/Solar/Pages/Intern/UpdateSpecific.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Solar.Pages.Intern
+{
+    public class UpdateSpecificModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
