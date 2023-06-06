2f8a3e0f4314d8204862c5e451cc3a1c8da52e82
philipv99 <philipv9913@gmail.com>
2023-06-06 14:51:34 +0200

    update site alost done

--- Solar/Solar/Pages/Intern/UpdateSpecific.cshtml.cs
+++ Solar/Solar/Pages/Intern/UpdateSpecific.cshtml.cs
@@ -5,24 +5,31 @@ namespace Solar.Pages.Intern
 {
     public class UpdateSpecificModel : PageModel
     {
+        private IDimensioningDataService _DimensioningDataService;
         private IProjectDataService ProjectDataService;
         private IRoofTypeDataService RoofTypeDataService;
 
         private int id { get; set; }
 
+
+        public List<Dimensioning> Dimensions { get; set; }
         public Project DataBaseInfo { get; set; }
         public RoofType RoofType { get; set; }
 
 
 
-        public UpdateSpecificModel(IProjectDataService projectDataService, IRoofTypeDataService roofTypeDataService)
+        public UpdateSpecificModel(IProjectDataService projectDataService, IRoofTypeDataService roofTypeDataService, IDimensioningDataService dimensioningDataService)
         {
             ProjectDataService = projectDataService;
             RoofTypeDataService = roofTypeDataService;
+
+            _DimensioningDataService = dimensioningDataService;
         }
 
         public void OnGet(int id)
         {
+            Dimensions = _DimensioningDataService.GetAll();
+
             DataBaseInfo = ProjectDataService.Read(id);
             RoofType = RoofTypeDataService.Read((int)DataBaseInfo.Assembly.RoofTypeId);
         }
