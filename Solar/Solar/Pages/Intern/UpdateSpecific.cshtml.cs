30183f5925cc7f2b3e5e4ff8133c3969b89c3606
Christian Robbers Rasmussen <crr@likehero.dk>
2023-06-08 13:47:52 +0200

    Updated update sepcific

--- Solar/Solar/Pages/Intern/UpdateSpecific.cshtml.cs
+++ Solar/Solar/Pages/Intern/UpdateSpecific.cshtml.cs
@@ -1,44 +1,46 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Solar.Pages.Intern
 {
+    [Authorize(Policy = "admin")]
     public class UpdateSpecificModel : PageModel
     {
-        private IDimensioningDataService _DimensioningDataService;
-        private IProjectDataService ProjectDataService;
-        private IRoofTypeDataService RoofTypeDataService;
+        private IDimensioningDataService _dimensioningDataService;
+        private IProjectDataService _projectDataService;
+        private IRoofTypeDataService _roofTypeDataService;
 
-        private int id { get; set; }
-
-
-        public List<Dimensioning> Dimensions { get; set; }
+        [BindProperty]
         public Project DataBaseInfo { get; set; }
         public RoofType RoofType { get; set; }
+        public List<Dimensioning> Dimensions { get; set; }
 
 
 
         public UpdateSpecificModel(IProjectDataService projectDataService, IRoofTypeDataService roofTypeDataService, IDimensioningDataService dimensioningDataService)
         {
-            ProjectDataService = projectDataService;
-            RoofTypeDataService = roofTypeDataService;
-
-            _DimensioningDataService = dimensioningDataService;
+            _projectDataService = projectDataService;
+            _dimensioningDataService = dimensioningDataService;
+            _roofTypeDataService = roofTypeDataService;
         }
 
         public void OnGet(int id)
         {
-            Dimensions = _DimensioningDataService.GetAll();
 
-            DataBaseInfo = ProjectDataService.Read(id);
-            RoofType = RoofTypeDataService.Read((int)DataBaseInfo.Assembly.RoofTypeId);
+            DataBaseInfo = _projectDataService.Read(id);
+
+            Dimensions = _dimensioningDataService.GetAll();
+
+            RoofType = _roofTypeDataService.Read((int)DataBaseInfo.Assembly.RoofTypeId);
         }
 
-        //public async Task<IActionResult> OnPost(int id)
-        //{
+        public async Task<IActionResult> OnPost(int id)
+        {
+            
+            _projectDataService.Update(DataBaseInfo);
 
-
-        //    return RedirectToPage($"/Intern/RequestsSpecific/{id}");
-        //}
+            return RedirectToPage("/Intern/Requests");
+        }
     }
 }
